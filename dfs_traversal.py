c46a3f8c7919d1aaf9d0a53f314f65ca835e8257
Pratheepan Bernier <108440591+PratheepanBernier@users.noreply.github.com>
2022-11-17 21:38:38 +0530

    Add files via upload

---
+++ dfs_traversal.py
@@ -0,0 +1,28 @@
+'''
+You are given a graph and a number x as input. Your task is to print the DFS traversal nodes of the input graph starting from node x. Complete the following function in order to give the required output.
+
+Input Format:
+The first line of input contains a list containing sets representing a graph. The second line of input contains the number x. 
+Output Format:
+Complete the function in order to return the required output.
+
+Sample Input 0:
+[[1,0],[2,0],[3,0],[3,2]]
+3
+
+Sample Output 0:
+3 0 2 
+'''
+
+def dfs_traversal(visited, graph, node):  
+    if node not in visited:
+        print (node,end=" ")
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs_traversal(visited, graph, neighbour)
+
+graph = [[1,0],[2,0],[3,0],[3,2]]
+visited = set() 
+node_start = int(input(""))
+dfs_traversal(visited, graph,node_start)
+ 
---
+++ string_palindromic_subsequence.py
@@ -0,0 +1,46 @@
+'''
+Given a string str, your task is to write a program which takes a string str as its only input and returns an integer denoting the no of palindromic subsequence (need not necessarily be distinct) which could be formed from the string str.
+
+Input Format:
+The first and only line of input contains string str.
+
+Output Format:
+The output will be an integer denoting the no of palindromic subsequence which could be formed from the string str.
+
+Sample Input :
+abcdef
+
+Sample Output :
+6
+
+Explanation:
+For string 'abcdef' palindromic subsequence are : "a" ,"b", "c" ,"d","e","f"
+'''
+
+
+def palindrome_subs_count(start_val, end_val):
+
+	if(start_val > end_val):
+		return 0
+
+	elif(string_check_arr[start_val][end_val] != -1):
+		return string_check_arr[start_val][end_val]
+
+	if(start_val == end_val):
+		string_check_arr[start_val][end_val] = 1
+		return string_check_arr[start_val][end_val]
+
+	elif (str[start_val] == str[end_val]):
+		string_check_arr[start_val][end_val] = (palindrome_subs_count(start_val + 1, end_val) +
+					palindrome_subs_count(start_val, end_val - 1) + 1)
+		return string_check_arr[start_val][end_val]
+	else:
+		string_check_arr[start_val][end_val] = (palindrome_subs_count(start_val + 1, end_val) +
+					palindrome_subs_count(start_val, end_val - 1) -
+					palindrome_subs_count(start_val + 1, end_val - 1))
+		return string_check_arr[start_val][end_val]
+
+str = input("")
+string_check_arr = [[-1 for x_value in range(1000)] for y_value in range(1000)]
+string_size = len(str)
+print(palindrome_subs_count(0, string_size - 1))
